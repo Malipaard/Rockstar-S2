d2d2b1c796f4f4d101c3479d5010b9ef9debf0cb
Jordy Rutjens <jordyrutjenspp@gmail.com>
2020-09-24 12:12:42 +0200

    Setup
    Setup of the application

---
+++ Volledig Project/Rockstars_ITtrainees/Rockstars_ITtrainees/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Rockstars_ITtrainees.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
