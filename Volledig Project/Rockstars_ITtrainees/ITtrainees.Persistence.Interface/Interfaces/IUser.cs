d84f79962d2a7f50874a8d9c3a561916872b5865
Pieter <zotje3000@gmail.com>
2020-10-07 12:06:58 +0200

    Eerste opzet van de Architectuur

---
+++ Volledig Project/Rockstars_ITtrainees/ITtrainees.Persistence.Interface/Interfaces/IUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ITtrainees.Persistence.Interface.Interfaces
+{
+    public interface IUser
+    {
+        int UserId { get; }
+        string FirstName { get; set; }
+        string LastName { get; set; }
+    }
+}
---
+++ Volledig Project/Rockstars_ITtrainees/Rockstars_ITtrainees/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ITtrainees.MVC.Models
+{
+    public class User
+    {
+        public int UserId { get; private set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
