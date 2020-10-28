18ed45c91dbb6dce313910b98b7bbe48aa970c87
Tmoemes <meesvanleusden@gmail.com>
2020-10-28 15:19:47 +0100

    random test artikelen

--- Volledig Project/Rockstars_ITtrainees/ITtrainees.App/HtmlConverter.cs
+++ Volledig Project/Rockstars_ITtrainees/ITtrainees.App/HtmlConverter.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.IO;
+//using Microsoft.Office.Interop.Word;
+using System.Text.RegularExpressions;
+
 
 namespace ITtrainees.Logic
 {
