68eeaf40fd28ab1de8add9dcb013889236122d54
CasLeeflang <466933@student.fontys.nl>
2020-11-11 11:53:17 +0100

    cahnged connection string

--- Volledig Project/Rockstars_ITtrainees/ITtrainees.Persistence/ArticlesContext.cs
+++ Volledig Project/Rockstars_ITtrainees/ITtrainees.Persistence/ArticlesContext.cs
@@ -20,7 +20,7 @@ namespace ITtrainees.DataAcces
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Data Source=LAPTOP-7SORRU5A; Integrated Security=SSPI; Initial Catalog=TechBurstArticles;");
+            optionsBuilder.UseSqlServer("Persist Security Info=False;User ID=CasLeeflang;Password=Plantenbak;Initial Catalog=TechBurstArticles;Server=145.93.120.32");
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
