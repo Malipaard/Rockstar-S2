7eb03f6e55855807e86c4962a89e1a729bc6b990
Pieter <zotje3000@gmail.com>
2020-11-11 12:24:21 +0100

    gestashte bestanden teruggehaald

--- Volledig Project/Rockstars_ITtrainees/Rockstars_ITtrainees/Proxy/ArticleOperations.cs
+++ Volledig Project/Rockstars_ITtrainees/Rockstars_ITtrainees/Proxy/ArticleOperations.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using 
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ITtrainees.MVC.Proxy
 {
