e5fb933d55a2498eacac9b565dc8178de5f992d5
CasLeeflang <466933@student.fontys.nl>
2020-11-26 14:17:24 +0100

    Added account class

---
+++ Volledig Project/Rockstars_ITtrainees/ITtrainees.Models/Account.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ITtrainees.Models
+{
+    public class Account
+    {
+        public int AccountID { get; set; }
+        public string Name { get; set; }
+        public int Points { get; set; }
+        public bool IsAdmin { get; set; }
+
+        public Account(int accountID, string name, int points, bool isAdmin)
+        {
+            this.AccountID = accountID;
+            this.Name = name;
+            this.Points = points;
+            this.IsAdmin = isAdmin;
+        }
+    }
+}
