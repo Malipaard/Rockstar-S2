9745d662d19a87e445c6318947aac7dc94107cad
Pieter <zotje3000@gmail.com>
2020-12-02 12:05:34 +0100

    committ

---
+++ Volledig Project/Rockstars_ITtrainees/Rockstars_ITtrainees/Models/Home/ArticleUploadViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ITtrainees.MVC.Models.Home
+{
+    public class ArticleUploadViewModel
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Summary { get; set; }
+        public string Tag { get; set; }
+        public string HeaderImage { get; set; }
+    }
+}
