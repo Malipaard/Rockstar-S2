e36d8d4a4cc56d38530c224fb9418011d379e023
SemWenders <66679237+SemWenders@users.noreply.github.com>
2020-12-03 13:27:15 +0100

    added questionAPI file

---
+++ Volledig Project/Rockstars_ITtrainees/ITtrainees.App/QuestionAPI.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ITtrainees.Logic
+{
+    [Route("api/question")]
+    [ApiController]
+    class QuestionAPI : ControllerBase
+    {
+
+    }
+}
