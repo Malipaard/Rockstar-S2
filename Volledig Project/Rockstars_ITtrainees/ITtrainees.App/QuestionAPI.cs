66dfdc64e9e99e032bf12824f7380c1bba997d86
SemWenders <66679237+SemWenders@users.noreply.github.com>
2020-12-09 13:36:31 +0100

    question api

--- Volledig Project/Rockstars_ITtrainees/ITtrainees.App/QuestionAPI.cs
+++ Volledig Project/Rockstars_ITtrainees/ITtrainees.App/QuestionAPI.cs
@@ -9,6 +9,10 @@ namespace ITtrainees.Logic
     [ApiController]
     class QuestionAPI : ControllerBase
     {
-
+        [HttpGet("{test1}/{test2}")]
+        private string test(int test1, string test2) 
+        {
+            return test1.ToString() + test2;
+        }
     }
 }
