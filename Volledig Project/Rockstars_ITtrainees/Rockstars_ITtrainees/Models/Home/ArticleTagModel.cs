d9b4761f332a2d64abc7d68f466dbdfa57863c20
Pieter <zotje3000@gmail.com>
2021-01-13 10:59:37 +0100

    .

---
+++ Volledig Project/Rockstars_ITtrainees/Rockstars_ITtrainees/Models/Home/ArticleTagModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ITtrainees.MVC.Models.Home
+{
+    public class ArticleTagModel
+    {
+        public int TagId { get; set; }
+        public string TagName { get; set; }
+        public bool IsSelected { get; set; }
+
+        public ArticleTagModel(int tagId, string tagName)
+        {
+            TagId = tagId;
+            TagName = tagName;
+        }
+        public ArticleTagModel()
+        {
+
+        }
+    }
+}
